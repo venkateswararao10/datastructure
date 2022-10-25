79f9897f1d266e2ea2eded4cb98e3b54fe54fcee
venkateswararao10 <104445024+venkateswararao10@users.noreply.github.com>
2022-10-25 10:57:29 +0530

    Update linearprobing.py

--- linearprobing.py
+++ linearprobing.py
@@ -9,7 +9,7 @@ def hash(stri):
        hashval=v-96
    sum=sum+hashval
  return sum
-print(hash("bye"))
+print(hash("djt"))
 def insertlinearprobing(stri,arr):
     key=hash(stri)
     for j in range(len(arr)):
@@ -17,14 +17,31 @@ def insertlinearprobing(stri,arr):
            if arr[ind]==-1:
              arr.insert(ind,stri)
              break
+def dellinearprobing(stri,arr):
+ for i in range(len(arr)):
+    if arr[i]==stri:
+        arr[i]=-1
+        print("its deleted")
+        return
+ print("string not found")
+def searchlinearprobing(stri,arr):
+    for i in range(len(arr)):
+     if arr[i]==stri:
+        print(f"{stri} is found")
+        return
+    print(f"{stri} is not found")
 s="abcd"
 s1="abcd"
 s2="hlo"
 s3="bye"
+s4="abcd"
 a=[-1 for i in range(50)]
 insertlinearprobing(s,a)
 insertlinearprobing(s1,a)
 insertlinearprobing(s2,a)
 insertlinearprobing(s3,a)
+searchlinearprobing(s4,a)
+searchlinearprobing("rag",a)
+dellinearprobing(s4,a)
+dellinearprobing("djt",a)
 print(a)
-    
